9c6528caf615b9f544c785589c9304b8fa24cd8c
sektor91 <sektor9130@gmail.com>
2025-05-21 13:21:32 -0500

    cambios

--- js/condicionales.js
+++ js/condicionales.js
@@ -1,4 +1,4 @@
-
+/* 
 let altura = parseFloat (prompt("digite la estatura"))
 
 if(altura<= 150){
@@ -9,10 +9,10 @@ if(altura<= 150){
 
 }else if(altura >= 171){
     alert("PERSONA DE ALTURA ALTA")
-}
+} */
 
 
-let matematicas = parseFloat(prompt(" digite la nota de matematicas "))
+/* let matematicas = parseFloat(prompt(" digite la nota de matematicas "))
 let español = parseFloat(prompt(" digite la nota de español "))
 let sociales = parseFloat(prompt(" digite la nota de sociales "))
 
@@ -21,12 +21,12 @@ let promedio = ((matematicas+español+sociales)/3)
 if (promedio === 10){
     alert("EXCELENTE")
 }
-else if (promedio > 7 && promedio < 9){
+else if (promedio >= 7 && promedio <= 9){
     alert("BUENO")
 }
-else if ( promedio <= 7){
+else if ( promedio < 7){
     alert("INSUFICIENTE")
-} 
+} */ 
 
 
 let estatura = parseInt(prompt("DIGITE SU ESTATURA"))
@@ -42,25 +42,25 @@ if(estatura >= 170 && velocidad >= 100 && edad >= 18){
 }
 
 
-let preciounitario = parseInt(prompt("INTRODUCE EL PRECIO DE ALGUN ESCRITORIO :"));
+/* let preciounitario = parseInt(prompt("INTRODUCE EL PRECIO DE ALGUN ESCRITORIO :"));
 let cantidad = parseInt(prompt("INTRODUCE LA CANTIDAD DE ESCRITORIOS QUE DESEA COMPRAR :"));
-let totalapagar = 0
+let totalapagar = preciounitario * cantidad
 let descuento = 0
 
 if(cantidad < 5){
-    descuento = 0,10
-    totalapagar = preciounitario - (preciounitario * descuento)
+    descuento = 0.10
+    totalapagar = totalapagar - (totalapagar * descuento)
     document.writeln("EL TOTAL A PAGAR ES DE ", totalapagar , "EL DESCUENTO FUE 10%")
 }
-else if(cantidad >= $ && cantidad < 10){
+else if(cantidad >= 5 && cantidad < 10){
     descuento = 0.28
-    totalapagar = preciounitario - (preciounitario * descuento)
+    totalapagar = totalapagar - (totalapagar * descuento)
     document.writeln("EL TOTAL A PAGAR ES DE ", totalapagar , "EL DESCUENTO ES DE 20%")
 
 }
 else if(cantidad >= 10){
     descuento = 0.40
-    totalapagar = preciounitario - (preciounitario * descuento)
+    totalapagar = totalapagar - (totalapagar * descuento)
     document.writeln("EL TOTAL A PAGAR ES DE ", totalapagar , "EL DESCUENTO ES DE 40%")
     
-}
+} */
